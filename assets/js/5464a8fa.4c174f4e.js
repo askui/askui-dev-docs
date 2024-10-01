1c85c5a08eedba4c98235b968408a773665e3276
JohannesDienst-askui <112638047+JohannesDienst-askui@users.noreply.github.com>
2024-10-01 07:35:55 +0000

    Deploying to gh-pages from @ askui/askui-dev-docs@6b33534ce577ad4d30916b3b9c7ac7b5e8aab514 🚀

---
+++ assets/js/5464a8fa.4c174f4e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[20584],{42144:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/release-notes","page":1,"postsPerPage":15,"totalPages":1,"totalCount":15,"blogDescription":"Blog","blogTitle":"Release Notes"}}')}}]);
